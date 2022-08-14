f1431d2636b73a5a54c9b5c7cc2c48e93fffbf0b
Saimon Lovell <staysynchronize@gmail.com>
2022-08-14 07:41:44 -0400

    new gem

---
+++ lib/render/text/helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "helper/version"
+
+module Render
+  module Text
+    module Helper
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/render/text/helper/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Render
+  module Text
+    module Helper
+      VERSION = "0.1.0"
+    end
+  end
+end
