3cecef06029641a8bfb3a413224cc6ffc94929a3
Saimon Lovell <staysynchronize@gmail.com>
2022-08-14 21:06:58 -0400

    adding string and boolean helper rspec

--- spec/render/text/helper/boolean_spec.rb
+++ spec/render/text/helper/boolean_spec.rb
@@ -18,4 +18,56 @@ RSpec.describe TrueClass do
       it { is_expected.to eq('y') }
     end
   end
+
+  describe '#to_yes_no' do
+    subject { boolean.to_yes_no(capital_letter: capital_letter) }
+
+    context 'with capital_letter being true' do
+      let(:capital_letter) { true }
+
+      it { is_expected.to eq('YES') }
+    end
+
+    context 'with capital_letter being false' do
+      let(:capital_letter) { false }
+
+      it { is_expected.to eq('yes') }
+    end
+  end
+end
+
+RSpec.describe FalseClass do
+  let(:boolean) { false }
+
+  describe '#to_yn' do
+    subject { boolean.to_yn(capital_letter: capital_letter) }
+
+    context 'with capital_letter being true' do
+      let(:capital_letter) { true }
+
+      it { is_expected.to eq('N') }
+    end
+
+    context 'with capital_letter being false' do
+      let(:capital_letter) { false }
+
+      it { is_expected.to eq('n') }
+    end
+  end
+
+  describe '#to_yes_no' do
+    subject { boolean.to_yes_no(capital_letter: capital_letter) }
+
+    context 'with capital_letter being true' do
+      let(:capital_letter) { true }
+
+      it { is_expected.to eq('NO') }
+    end
+
+    context 'with capital_letter being false' do
+      let(:capital_letter) { false }
+
+      it { is_expected.to eq('no') }
+    end
+  end
 end
